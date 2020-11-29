cb00d20aecb319a3c2eed3e1fc335446e4b4323d
Auri <me@auri.xyz>
2020-11-28 17:39:52 -0800

    Add health mod, re-namespace aurailus mods to auri, new structure format which is kinda broke.

--- script/init.lua
+++ script/init.lua
@@ -1,5 +1,16 @@
 _G['minetest'] = {}
 
+-- local global_meta = {
+-- 	__newindex = function(tbl, key, val)
+-- 		print("HI!")
+-- 		dump(key)
+-- 		dump(val)
+-- 	end,
+-- 	__index = getmetatable(_G)
+-- }
+
+-- setmetatable(_G, global_meta)
+
 minetest.__usertype = {}
 
 runfile(_PATH .. "usertype/itemstack")
@@ -10,4 +21,5 @@ minetest.__util = {}
 
 runfile(_PATH .. "util/identifier")
 
-runfile(_PATH .. "api")
+runfile(_PATH .. "api/base")
+runfile(_PATH .. "api/register")
--- script/util/identifier.lua
+++ script/util/identifier.lua
@@ -1,9 +1,15 @@
 minetest.__util.identifier = {}
 
 function minetest.__util.identifier.encode(identifier)
-	return identifier:sub(identifier:find(':') + 1, #identifier)
+	local split = identifier:find(':')
+	if not split then return identifier end
+	return identifier:sub(split + 1, #identifier)
 end
 
 function minetest.__util.identifier.decode(identifier)
+	local split = identifier:find(':')
+	if not split then return identifier end
+	
+	if identifier == '' then return '' end
 	return 'minetest:' .. identifier
 end
